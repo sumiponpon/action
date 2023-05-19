0411ca65cd4bc12ad8528eb488489550664f051d
y-sumida <y-sumida@ceres-inc.jp>
2023-05-19 19:37:52 +0900

    test_comit

---
+++ test/employee.php
@@ -0,0 +1,39 @@
+<?php
+
+class RegularEmployee
+{
+    /**
+     * @var int
+     */
+    private $employee_id;
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @param $employee_id
+     * @param $name
+     */
+    public function __construct($employee_id, $name)
+    {
+        $this->employee_id = $employee_id;
+        $this->name = $name;
+    }
+    /**
+     * @return int
+     */
+    public function getEmployeeId(): mixed
+    {
+        return $this->employee_id;
+    }
+    /**
+     * @return mixed
+     */
+    public function getName(): mixed
+    {
+        return $this->name;
+    }
+}
+$employee = new RegularEmployee("1","山田");
+echo $employee->getEmployeeId();
+echo $employee->getName();
